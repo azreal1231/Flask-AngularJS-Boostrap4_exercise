6bbf2d23e504c7e2852714a58b5b805f8a9391d8
Edward <edwardjack91@gmail.com>
2020-08-07 18:08:03 +0200

    ? Added new view to main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import json
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+
+@app.route("/get-data", methods=['GET', 'POST'])
+def getdata():
+    import rpc
+    method_name = request.form.get('de')
+    params = json.loads(request.form.get('params'))
+
+    Records = rpc.returnUsers(method_name, params)
+    response = {'Result': 'OK', 'Records': Records}
+    response = json.dumps(response)
+    return response
 
 
 if __name__ == "__main__":
